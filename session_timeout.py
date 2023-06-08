aa33826cae5d6a4cd1c461a6d4c760c7bfe4ec9b
Grigory Vakulenchuk <github@mael.is>
2023-06-08 07:06:17 -0700

    updated time difference function

--- session_timeout.py
+++ session_timeout.py
@@ -28,31 +28,40 @@ def get_last_activity():
         print("Request failed with status code:", response.status_code)
 print(get_last_activity())
 
-def get_active_sessions():
-    """make api call to get and return sessionid, userid and last_activity"""
-    active_sessions = []
-    response = requests.get(url = URL+'/Sessions', headers=headers, timeout=5)
-    if response.status_code == 200:
-        resp_data = response.json()
-        if resp_data:
-            for element in resp_data:
-                active_sessions.append((element['UserId'], element['LastActivityDate']))
-            return active_sessions
-        else:
-            print("Empty list received from the API.")
-    else:
-        print("Request failed with status code:", response.status_code)
-print(get_active_sessions())
+# def get_active_sessions():
+#     """make api call to get and return sessionid, userid and last_activity"""
+#     active_sessions = []
+#     response = requests.get(url = URL+'/Sessions', headers=headers, timeout=5)
+#     if response.status_code == 200:
+#         resp_data = response.json()
+#         if resp_data:
+#             for element in resp_data:
+#                 active_sessions.append((element['UserId'], element['LastActivityDate']))
+#             return active_sessions
+#         else:
+#             print("Empty list received from the API.")
+#     else:
+#         print("Request failed with status code:", response.status_code)
+# print(get_active_sessions())
 
-def get_current_time():
+def compare_time(users_list):
+    """compare active time with current time"""
+    
     current_time = datetime.datetime.now().isoformat()
-    return current_time
-
-current_time = get_current_time()
-print(current_time)
-
+    active_users = []
+    
+    for user_tuple in users_list:
+        user_id, active_time = user_tuple
+        active_time = datetime.date.fromisoformat(active_time)
+        time_difference = current_time - active_time
+    if time_difference > timedelta(minutes=30):
+        active_users.append(user_id)
+    return active_users
 
 def logout_idlers():
     """compare the last active time with the current time, if it has been more than 30m, logout this user"""
-    
+    last_activity_tuples = get_last_activity()
+    for tuple in last_activity_tuples:
+        idlers = compare_time(last_activity_tuples)
 
+logout_idlers()
